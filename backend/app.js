a751c6f838ff33b9f3a3d09b1908149adff2b855
Max Bondarenko <jaderystudion@gmail.com>
2024-03-10 14:53:58 +0200

    Added logic to calculate endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+
 import errorHandler from './utils/errorHandler.js'
-import catchAsync from './utils/catchAsync.js'
 import notFound from './utils/notFound.js'
+import { calculate } from './controllers/calculate.controller.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors({ origin: ['http://localhost:5173'] }))
 
-app.use(
-    '/api/calculate',
-    catchAsync(async (req, res, next) => {
-        res.status(200).json('success')
-    })
-)
+app.use('/api/calculate', calculate)
 
 app.use(notFound)
 app.use(errorHandler)
