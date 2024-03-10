623c27c692d52505b3e90b6e84f3767192896995
Max Bondarenko <jaderystudion@gmail.com>
2024-03-10 13:01:49 +0200

    Added error handling and api route for doing calculations to server

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import cors from 'cors'
+import errorHandler from './utils/errorHandler.js'
+import catchAsync from './utils/catchAsync.js'
+import notFound from './utils/notFound.js'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors({ origin: ['localhost:5173'] }))
+app.use(cors({ origin: ['http://localhost:5173'] }))
+
+app.use(
+    '/api/calculate',
+    catchAsync(async (req, res, next) => {
+        res.status(200).json('success')
+    })
+)
+
+app.use(notFound)
+app.use(errorHandler)
 
 export default app
